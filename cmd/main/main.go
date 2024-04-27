34f05c2d00be95e7b349b4edf0b730d5293c9e6b
Hardikmaind <maindhardik@gmail.com>
2024-04-27 14:41:27 +0530

    added air package to auto reload and also added the api logging

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,11 +6,26 @@ import(
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/akhil/go-bookstore/pkg/routes"
+	"github.com/gorilla/handlers"
+
 )
 
 func main(){
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
+
+
+	//this is the CORS middleware
+	 // CORS middleware
+	 corsHandler := handlers.CORS(
+        handlers.AllowedOrigins([]string{"*"}),
+        handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+        handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+    )
+
+
+
+	http.Handle("/",corsHandler(r))
+	// log.Fatal(http.ListenAndServe("0.0.0.0:9010", r))
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/akhil/go-bookstore
 go 1.16
 
 require (
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 )
